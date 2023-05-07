521fdc0332e04f13170daceee5e74029b10c44af
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-07 17:27:39 +0200

    xxx

--- src/train.py
+++ src/train.py
@@ -8,12 +8,12 @@ def base_training(trainer, dm, lit_mod, test_dm=None, test_fn=None, ckpt=None):
         print()
 
     lit_mod.set_norm_stats = dm.norm_stats()
-    trainer.fit(lit_mod, datamodule=dm, ckpt_path=ckpt)
+    #trainer.fit(lit_mod, datamodule=dm, ckpt_path=ckpt)
 
     if test_fn is not None:
         if test_dm is None:
             test_dm = dm
-        lit_mod.norm_stats = test_dm.norm_stats()
+        #lit_mod.norm_stats = test_dm.norm_stats()
 
         best_ckpt_path = trainer.checkpoint_callback.best_model_path
         trainer.callbacks = []
