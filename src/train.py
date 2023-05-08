91659cf39abbabd4c3b8aa54bf26e845ed45de55
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-08 22:11:47 +0200

    xxx

--- src/train.py
+++ src/train.py
@@ -10,10 +10,12 @@ def base_training(trainer, dm, lit_mod, test_dm=None, test_fn=None, ckpt=None):
     #trainer.fit(lit_mod, datamodule=dm, ckpt_path=ckpt)
     
     lit_mod.set_norm_stats = dm.norm_stats()
-    #lit_mod.load_from_checkpoint('outputs/2023-05-07/22-59-30/base_l63/checkpoints/val_mse=0.6534-epoch=379.ckpt')
     
     print()
     print('.......................')
+    print(lit_mod.model.model_Grad.lstm.Gates.weight[0,0,:,:])
+    print()
+    lit_mod.load_from_checkpoint('outputs/2023-05-07/22-59-30/base_l63/checkpoints/val_mse=0.6534-epoch=379.ckpt')
     print(lit_mod.model.model_Grad.lstm.Gates.weight[0,0,:,:])
     print()
     
