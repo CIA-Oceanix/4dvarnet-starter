619f583dc3de5d81b726f200358775ea1ef3387c
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-08 21:33:51 +0200

    xxx

--- src/train.py
+++ src/train.py
@@ -10,7 +10,7 @@ def base_training(trainer, dm, lit_mod, test_dm=None, test_fn=None, ckpt=None):
     #trainer.fit(lit_mod, datamodule=dm, ckpt_path=ckpt)
     
     lit_mod.set_norm_stats = dm.norm_stats()
-    lit_mod.load_from_checkpoint('/homes/rfablet/Home2/Research/Python/Git_rfablet/4dvarnet-starter/outputs/2023-05-07/22-59-30/base_l63/checkpoints/val_mse=0.6534-epoch=379.ckpt')
+    lit_mod.load_from_checkpoint('xxxxxx.ckpt')#'/homes/rfablet/Home2/Research/Python/Git_rfablet/4dvarnet-starter/outputs/2023-05-07/22-59-30/base_l63/checkpoints/val_mse=0.6534-epoch=379.ckpt')
 
     trainer.test(lit_mod, dataloaders=dm.val_dataloader())#, ckpt_path=ckpt)
     trainer.test(lit_mod, dataloaders=dm.test_dataloader())#, ckpt_path=ckpt)
