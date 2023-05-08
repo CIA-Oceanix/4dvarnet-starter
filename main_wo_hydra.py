a6b8ebc2e37d508cd62545aef05988676a3fe889
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-08 18:27:25 +0200

    xxx

--- main_wo_hydra.py
+++ main_wo_hydra.py
@@ -33,12 +33,18 @@ print(OmegaConf.to_yaml(cfg))
 dm = BaseDataModule(create_l63_datasets(cfg.datamodule.input_data.param_dataset),cfg.datamodule.param_datamodule)
 
 mod = Lit4dVarNet_L63(cfg.model.params,patch_weight=get_constant_crop_l63(patch_dims=cfg.model.patch_weight.patch_dims,crop=cfg.model.patch_weight.crop))
-
+print()
+print('.................')
+print(mod.model.model_VarCost.alphaObs)
 #mod.load_from_checkpoint('outputs/2023-05-07/22-59-30/base_l63/checkpoints/val_mse=0.6534-epoch=379.ckpt')
 
 mod.load_from_checkpoint('resL63/exp02-new/model-l63-jamesDim0_08_20unet-exp02-new-Noise02-igrad10_02-dgrad100-drop20-rnd-init00-lstm-init00-epoch=30-val_loss=1.31.ckpt')
 
+print()
+print()
 print(mod.hparams)
+print('.................')
+print(mod.model.model_VarCost.alphaObs)
 
 mod.set_norm_stats = dm.norm_stats()
 
