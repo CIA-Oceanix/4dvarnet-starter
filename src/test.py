0d7f0bffae1ab530044b78816d57b51784c6787b
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-09 12:42:45 +0200

    xxx

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ import numpy as np
 
 torch.set_float32_matmul_precision('high')
 
-def base_testing(trainer, dm, lit_mod,chkpt):
+def base_testing(trainer, dm, lit_mod,ckpt):
     if trainer.logger is not None:
         print()
         print("Logdir:", trainer.logger.log_dir)
@@ -17,7 +17,7 @@ def base_testing(trainer, dm, lit_mod,chkpt):
     trainer.callbacks = []
     lit_mod.set_norm_stats = dm.norm_stats()
     
-    trainer.test(lit_mod, dataloaders=dm.val_dataloader(),ckpt_path=chkpt)
+    trainer.test(lit_mod, dataloaders=dm.val_dataloader(),ckpt_path=ckpt)
 
     X_train, x_train, mask_train, x_train_Init, x_train_obs = dm.input_data[0]    
     idx_val = X_train.shape[0]-500
