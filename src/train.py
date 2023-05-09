0ef24715b6bcccd1444c88c007206734114a66ce
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-09 12:18:25 +0200

    xxx

--- src/train.py
+++ src/train.py
@@ -7,51 +7,10 @@ def base_training(trainer, dm, lit_mod, test_dm=None, test_fn=None, ckpt=None):
         print("Logdir:", trainer.logger.log_dir)
         print()
 
-    #trainer.fit(lit_mod, datamodule=dm, ckpt_path=ckpt)
+    trainer.fit(lit_mod, datamodule=dm, ckpt_path=ckpt)
     
     lit_mod.set_norm_stats = dm.norm_stats()
-    
-    print()
-    print('.......................')
-    
-    print()
-    print()
-    
-    #print(lit_mod.model.model_Grad.lstm.Gates.weight[0,0,:,:],flush=True)
-    #print(lit_mod)
-    lit_mod.load_from_checkpoint('outputs/2023-05-07/22-59-30/base_l63/checkpoints/val_mse=0.6534-epoch=379.ckpt')
-    print(lit_mod.hparams)
-#    print('.... eps = %f'%lit_mod.model.eps, flush=True)
-    print(lit_mod.model.model_VarCost.alphaObs)
-    print(lit_mod.model.model_VarCost.alphaReg)
-    print(lit_mod.model.model_VarCost.WObs)
-    #print(lit_mod.model.model_Grad.lstm.Gates.weight[0,0,:,:],flush=True)
-    #print('.... %f  -- %f -- %f'%(lit_mod.model.lr_grad,lit_mod.model.lr_rnd,lit_mod.model.model_Grad.sig_lstm_init),flush=True)
-    #print('.... %d  -- %d -- %d'%(lit_mod.model.n_grad,lit_mod.hparams.k_n_grad,lit_mod.model.n_step),flush=True)
-    #print()
-  
-    trainer.test(lit_mod, dataloaders=dm.val_dataloader())#, ckpt_path=ckpt)
-    #print('.... eps = %f'%lit_mod.model.eps, flush=True)
-    print()
-    print(lit_mod.model.model_VarCost.alphaObs)
-    print(lit_mod.model.model_VarCost.alphaReg)
-    print(lit_mod.model.model_VarCost.WObs)
-    print('.... %f  -- %f -- %f'%(lit_mod.model.lr_grad,lit_mod.model.lr_rnd,lit_mod.model.model_Grad.sig_lstm_init),flush=True)
-    print('.... %d  -- %d -- %d'%(lit_mod.model.n_grad,lit_mod.hparams.k_n_grad,lit_mod.model.n_step),flush=True)
-    #print(lit_mod)
-    
-    #print(lit_mod.model.model_Grad.lstm.Gates.weight[0,0,:,:])
-    
-    trainer.test(lit_mod, dataloaders=dm.test_dataloader(), ckpt_path=ckpt)
-    print()
-    #print('.... eps = %f'%lit_mod.model.eps, flush=True)
-    print()
-    print(lit_mod.model.model_VarCost.alphaObs)
-    print(lit_mod.model.model_VarCost.alphaReg)
-    print(lit_mod.model.model_VarCost.WObs)
-    print('.... %f  -- %f -- %f'%(lit_mod.model.lr_grad,lit_mod.model.lr_rnd,lit_mod.model.model_Grad.sig_lstm_init),flush=True)
-    print('.... %d  -- %d -- %d'%(lit_mod.model.n_grad,lit_mod.hparams.k_n_grad,lit_mod.model.n_step),flush=True)
-    
+        
     if test_fn is not None:
         if test_dm is None:
             test_dm = dm
