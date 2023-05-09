58656174da491d9cb881eba0f0a3ad3888a2d454
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-09 11:13:46 +0200

    xxx

--- main_wo_hydra.py
+++ main_wo_hydra.py
@@ -34,11 +34,10 @@ dm = BaseDataModule(create_l63_datasets(cfg.datamodule.input_data.param_dataset)
 
 if 1*0 :
     mod = Lit4dVarNet_L63(cfg.model.params,patch_weight=get_constant_crop_l63(patch_dims=cfg.model.patch_weight.patch_dims,crop=cfg.model.patch_weight.crop))
+else:
     #mod.load_from_checkpoint('outputs/2023-05-07/22-59-30/base_l63/checkpoints/val_mse=0.6534-epoch=379.ckpt')
-
     ckpt = 'resL63/exp02-new/model-l63-jamesDim0_08_20unet-exp02-new-Noise02-igrad05_02-dgrad100-drop20-rnd-init00-lstm-init00-epoch=01-val_loss=4.86.ckpt'
 #ckpt = 'resL63/exp02-new/model-l63-jamesDim0_08_20unet-exp02-new-Noise02-igrad05_02-dgrad100-drop20-rnd-init00-lstm-init00-epoch=01-val_loss=7.14.ckpt'
-else:
     mod = Lit4dVarNet_L63.load_from_checkpoint(ckpt)
 
 print()
