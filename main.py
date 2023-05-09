07960af751726d2b1957a6462f7ba61722e30ca2
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-09 22:53:45 +0200

    xxx

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import hydra
+#from omegaconf import OmegaConf
 
 @hydra.main(config_path='config', config_name='main', version_base='1.2')
 def main(cfg):
+    #OmegaConf.resolve(cfg)
     hydra.utils.call(cfg.entrypoints)
 
 if __name__ == '__main__':
