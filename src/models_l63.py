d72ecdeaef2acfde1ecf23f665c817547e3da35b
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-12 20:36:57 +0200

    xxx

--- src/models_l63.py
+++ src/models_l63.py
@@ -79,7 +79,12 @@ def create_filename_ckpt(suffix,params_data,params_model):
     print(params_data)
     print(params_model)
     
-    filename_chkpt = 'model-l63-' + params_model.solver +'-'
+    filename_chkpt = 'model-l63-' + params_model.solver +'-' 
+    if params_model.degradation_operator == 'no-degradation' :
+        filename_chkpt = filename_chkpt +  params_model.solver +'-' 
+    else:
+        filename_chkpt = filename_chkpt +  params_model.solver +'degrad-' 
+        
     filename_chkpt = filename_chkpt + params_data.genSuffixObs 
     filename_chkpt = filename_chkpt + '-Obs%02d'%params_data.sampling_step + '-Noise%02d'%(params_data.varNoise)        
     filename_chkpt = filename_chkpt + '-' + params_model.phi_param
