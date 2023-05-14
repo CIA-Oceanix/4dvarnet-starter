bddf546b1b3d8fc65c8c28fbbc57f0a66f87cbc6
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-14 22:18:14 +0200

    xxx

--- src/solver_l63.py
+++ src/solver_l63.py
@@ -191,7 +191,7 @@ def compute_WeightedL2Norm1D(x2,w):
 
 # Gradient-based minimization using a LSTM using a (sub)gradient as inputs    
 class model_Grad_with_lstm(torch.nn.Module):
-    def __init__(self,ShapeData,periodicBnd=False,DimLSTM=0,rateDropout=0.,padding_mode='zeros',sig_lstm_init=0.):
+    def __init__(self,ShapeData,periodicBnd=False,DimLSTM=0,rateDropout=0.,padding_mode='zeros',sig_lstm_init=0.,dim_state_out=None):
         super(model_Grad_with_lstm, self).__init__()
 
         with torch.no_grad():
@@ -204,6 +204,11 @@ class model_Grad_with_lstm(torch.nn.Module):
             if( (self.PeriodicBnd == True) & (len(self.shape) == 2) ):
                 print('No periodic boundary available for FxTime (eg, L63) tensors. Forced to False')
                 self.PeriodicBnd = False
+
+            if dim_state_out == None :
+                self.dim_shape_out = self.shape[0]
+            else:
+                self.dim_shape_out = self.dim_shape_out
 
         self.convLayer     = self._make_ConvGrad()
         K = torch.Tensor([0.1]).view(1,1,1,1)
@@ -221,10 +226,10 @@ class model_Grad_with_lstm(torch.nn.Module):
         layers = []
 
         if len(self.shape) == 2: ## 1D Data
-            layers.append(torch.nn.Conv1d(self.DimState, self.shape[0], 1, padding=0,bias=False))
+            layers.append(torch.nn.Conv1d(self.DimState, self.dim_shape_out, 1, padding=0,bias=False))
             #layers.append(torch.nn.Conv1d(self.DimState, self.shape[0], 1, padding=0,bias=False))
         elif len(self.shape) == 3: ## 2D Data
-            layers.append(torch.nn.Conv2d(self.DimState, self.shape[0], (1,1), padding=0,bias=False))
+            layers.append(torch.nn.Conv2d(self.DimState, self.dim_shape_out, (1,1), padding=0,bias=False))
             #layers.append(torch.nn.Conv2d(self.shape[0]+self.DimState, self.shape[0], (1,1), padding=0,bias=False))
 
         return torch.nn.Sequential(*layers)
@@ -267,15 +272,11 @@ class model_Grad_with_lstm(torch.nn.Module):
         return grad,hidden_,cell_
 
 class model_Grad_with_lstm_and_state(torch.nn.Module):
-    def __init__(self,ShapeDataIn,periodicBnd=False,DimLSTM=0,rateDropout=0.,padding_mode='zeros',sig_lstm_init=0.,dim_state_out=None):
+    def __init__(self,ShapeDataIn,periodicBnd=False,DimLSTM=0,rateDropout=0.,padding_mode='zeros',sig_lstm_init=0.):
         super(model_Grad_with_lstm_and_state, self).__init__()
 
         with torch.no_grad():
             self.shape     = ShapeDataIn
-            if dim_state_out == None :
-                self.dim_shape_out = self.shape[0]
-            else:
-                self.dim_shape_out = self.dim_shape_out
             
             if DimLSTM == 0 :
                 self.DimState  = 5*self.shape[0]
@@ -302,10 +303,10 @@ class model_Grad_with_lstm_and_state(torch.nn.Module):
         layers = []
 
         if len(self.shape) == 2: ## 1D Data
-            layers.append(torch.nn.Conv1d(self.DimState, self.dim_shape_out, 1, padding=0,bias=False))
+            layers.append(torch.nn.Conv1d(self.DimState, self.shape[0], 1, padding=0,bias=False))
             #layers.append(torch.nn.Conv1d(self.DimState, self.shape[0], 1, padding=0,bias=False))
         elif len(self.shape) == 3: ## 2D Data
-            layers.append(torch.nn.Conv2d(self.DimState, self.dim_shape_out, (1,1), padding=0,bias=False))
+            layers.append(torch.nn.Conv2d(self.DimState, self.shape[0], (1,1), padding=0,bias=False))
             #layers.append(torch.nn.Conv2d(self.shape[0]+self.DimState, self.shape[0], (1,1), padding=0,bias=False))
 
         return torch.nn.Sequential(*layers)
