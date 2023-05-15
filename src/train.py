916fdfd4b01ecde2ac0168e5b388020df9e7f99f
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-15 22:46:36 +0200

    xxx

--- src/train.py
+++ src/train.py
@@ -7,8 +7,35 @@ def base_training(trainer, dm, lit_mod, test_dm=None, test_fn=None, ckpt=None):
         print("Logdir:", trainer.logger.log_dir)
         print()
 
-    lit_mod.set_norm_stats = dm.norm_stats()    
+    lit_mod.set_norm_stats = dm.norm_stats()
+    
     trainer.fit(lit_mod, datamodule=dm, ckpt_path=ckpt)
+
+            
+    if test_fn is not None:
+        if test_dm is None:
+            test_dm = dm
+        #lit_mod.norm_stats = test_dm.norm_stats()
+
+        best_ckpt_path = trainer.checkpoint_callback.best_model_path
+        trainer.callbacks = []
+        trainer.test(lit_mod, datamodule=test_dm, ckpt_path=best_ckpt_path)
+
+        print("\nBest ckpt score:")
+        print(test_fn(lit_mod).to_markdown())
+        print("\n###############")
+
+def fine_tuning(trainer, dm, lit_mod, test_dm=None, test_fn=None, ckpt=None):
+    if trainer.logger is not None:
+        print()
+        print("Logdir:", trainer.logger.log_dir)
+        print()
+
+    lit_mod.set_norm_stats = dm.norm_stats()  
+    if ckpt is not None:
+        lit_mod.load_state_dict(torch.load(ckpt)['state_dict'])
+    trainer.fit(lit_mod, datamodule=dm)
+
             
     if test_fn is not None:
         if test_dm is None:
