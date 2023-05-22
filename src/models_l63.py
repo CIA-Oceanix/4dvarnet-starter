52633be2afac442cda91d209753169fc8b2f308e
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-22 23:20:28 +0200

    xxx

--- src/models_l63.py
+++ src/models_l63.py
@@ -794,7 +794,7 @@ class Lit4dVarNet_L63(pl.LightningModule):
             #loss = 1.0 - torch.sqrt( torch.nanmean( dx * var_cost_grad / ( n_dx * n_grad ) )**2 + 1e-6 ) 
             
             
-            print(' %f -- %f'%(loss.detach().cpu().numpy(), torch.nanmean( dx * var_cost_grad / ( n_dx * n_grad ) ).detach().cpu().numpy() ) )
+            print('%f -- %f --  %f -- %f'%(torch.mean( dx**2 ).detach().cpu().numpy(),torch.mean( var_cost_grad**2 ).detach().cpu().numpy(),loss.detach().cpu().numpy(), torch.nanmean( dx * var_cost_grad / ( n_dx * n_grad ) ).detach().cpu().numpy() ) )
             #print( torch.sqrt( torch.mean( dx**2 )) )
             #print( torch.sqrt( torch.mean( var_cost_grad**2 )) )
         return loss
