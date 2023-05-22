061d40953c6232d38e338dc1f5ac770e262133f8
Quentin Febvre <quentin.febvre@imt-atlantique.fr>
2023-05-22 15:43:47 +0000

    Remove nonlinear mm

--- contrib/multimodal/__init__.py
+++ contrib/multimodal/__init__.py
@@ -14,36 +14,24 @@ MultiModalSSTTrainingItem = collections.namedtuple(
 
 
 def load_data_with_sst(obs_var='five_nadirs'):
-    inp = xr.open_dataset(
-        "../sla-data-registry/CalData/cal_data_new_errs.nc"
-    )[obs_var]
-    gt = (
-        xr.open_dataset(
+    inp = xr.open_dataset( "../sla-data-registry/CalData/cal_data_new_errs.nc")[obs_var]
+    gt = ( xr.open_dataset(
             "../sla-data-registry/NATL60/NATL/ref_new/NATL60-CJM165_NATL_ssh_y2013.1y.nc"
-        )
-        .ssh.isel(time=slice(0, -1))
+        ).ssh.isel(time=slice(0, -1))
         .interp(lat=inp.lat, lon=inp.lon, method="nearest")
     )
-
-    sst = (
-        xr.open_dataset(
+    sst = (xr.open_dataset(
             "../sla-data-registry/NATL60/NATL/ref_new/NATL60-CJM165_NATL_sst_y2013.1y.nc"
-        )
-        .sst.isel(time=slice(0, -1))
+        ).sst.isel(time=slice(0, -1))
         .interp(lat=inp.lat, lon=inp.lon, method="nearest")
     )
-
     ds =  (
         xr.Dataset(dict(
-            input=inp,
-            tgt=(gt.dims, gt.values),
-            sst=(sst.dims, sst.values)
+            input=inp, tgt=(gt.dims, gt.values), sst=(sst.dims, sst.values)
         ), inp.coords).load()
         .transpose('time', 'lat', 'lon')
     )
     return ds.to_array()
-
-
 
 class MultiModalDataModule(src.data.BaseDataModule):
     def post_fn(self):
@@ -77,36 +65,3 @@ class MultiModalObsCost(nn.Module):
             self.conv_sst(batch.sst.nan_to_num()),
         )
         return ssh_cost + sst_cost
-
-class NonLinearMultiModalObsCost(nn.Module):
-    def __init__(self, dim_in, dim_hidden):
-        super().__init__()
-        self.base_cost = src.models.BaseObsCost()
-        conv = lambda i, o: torch.nn.Conv2d(i, o, (3, 3), padding=1, bias=False)
-        self.head = torch.nn.Sequential(
-            torch.nn.ReLU(),
-            conv(dim_hidden, dim_hidden),
-            torch.nn.Tanh(),
-            conv(dim_hidden, dim_hidden),
-            torch.nn.BatchNorm2d(dim_hidden, track_running_stats=False)
-        )
-        self.leg_ssh = torch.nn.Sequential(
-            conv(dim_in, 2*dim_hidden),
-            torch.nn.ReLU(),
-            conv(2*dim_hidden, dim_hidden),
-        )
-
-        self.leg_sst = torch.nn.Sequential(
-            conv(dim_in, 2*dim_hidden),
-            torch.nn.ReLU(),
-            conv(2*dim_hidden, dim_hidden),
-        ) 
-
-    def forward(self, state, batch):
-        ssh_cost =  self.base_cost(state, batch)
-        sst_cost =  torch.nn.functional.mse_loss(
-            self.head(self.leg_ssh(state)),
-            self.head(self.leg_sst(batch.sst.nan_to_num())),
-        )
-        return ssh_cost + sst_cost
-
