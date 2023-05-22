4d5b667bd845aa79e6f15d04d1e72893332f0959
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-22 23:41:23 +0200

    xxx

--- src/models_l63.py
+++ src/models_l63.py
@@ -793,8 +793,7 @@ class Lit4dVarNet_L63(pl.LightningModule):
             loss = 1.0 - torch.nanmean( dx * var_cost_grad / ( n_dx * n_grad ) )  
             #loss = 1.0 - torch.sqrt( torch.nanmean( dx * var_cost_grad / ( n_dx * n_grad ) )**2 + 1e-6 ) 
             
-            
-            print('%e -- %e -- %e --  %f -- %f'%(torch.mean( f_x_1**2 ).detach().cpu().numpy(),torch.mean( dx**2 ).detach().cpu().numpy(),torch.mean( var_cost_grad**2 ).detach().cpu().numpy(),loss.detach().cpu().numpy(), torch.nanmean( dx * var_cost_grad / ( n_dx * n_grad ) ).detach().cpu().numpy() ) )
+            #print('%e -- %e -- %e --  %f -- %f'%(torch.sqrt(torch.mean( f_x_1**2 )).detach().cpu().numpy(),torch.mean( dx**2 ).detach().cpu().numpy(),torch.mean( var_cost_grad**2 ).detach().cpu().numpy(),loss.detach().cpu().numpy(), torch.nanmean( dx * var_cost_grad / ( n_dx * n_grad ) ).detach().cpu().numpy() ) )
             #print( torch.sqrt( torch.mean( dx**2 )) )
             #print( torch.sqrt( torch.mean( var_cost_grad**2 )) )
         return loss
