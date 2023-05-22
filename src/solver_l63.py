75f04b0dd25580e69cc223d42e490d5449acd7d8
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-22 07:38:43 +0200

    xxx

--- src/solver_l63.py
+++ src/solver_l63.py
@@ -669,14 +669,13 @@ class GradSolver_with_state_rnd(nn.Module):
             alpha_step_lstm = 1. / (iter + 1) 
         elif self.type_step_lstm == 'linear-relu' :
             alpha_step_lstm = torch.relu( torch.Tensor( [1. / (iter + 1) - 1. / (self.param_lstm_step + 1) ]) ).to(device)
-            print(alpha_step_lstm)
         elif self.type_step_lstm == 'sigmoid' :
             alpha_step_lstm = np.exp(-1. * iter / self.param_lstm_step )
             alpha_step_lstm = alpha_step_lstm / ( 1. + alpha_step_lstm )
 
-        #print()
-        #print( torch.sqrt( torch.mean( (alpha_step_lstm * grad_update)**2 ) ))
-        #print( torch.sqrt( torch.mean( (self.lr_grad * (iter + 1) / self.n_step * var_cost_grad)**2 ) ))
+        print()
+        print( torch.sqrt( torch.mean( (alpha_step_lstm * grad_update)**2 ) ))
+        print( torch.sqrt( torch.mean( (self.lr_grad * (iter + 1) / self.n_step * var_cost_grad)**2 ) ))
 
         state_update = (
             alpha_step_lstm * grad_update
