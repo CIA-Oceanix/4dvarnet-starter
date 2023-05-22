359130209e120cd87b0115b8f3a444279e7e2424
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-22 23:33:15 +0200

    xxx

--- src/models_l63.py
+++ src/models_l63.py
@@ -646,7 +646,7 @@ class Lit4dVarNet_L63(pl.LightningModule):
         x_ = kornia.filters.gaussian_blur2d(x, (3, 1), (1.0, 1.))
         
 
-        dx = x + self.hparams.gamma_degradation * (x_ - x)
+        dx = self.hparams.gamma_degradation * (x_ - x)
         #x = kornia.filters.median_blur(x, (3, 1))
         print()
         print( torch.mean(dx**2) )
