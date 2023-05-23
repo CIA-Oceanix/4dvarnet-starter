0febbc33b3cc9fb9cca8a3ccca9ed76aaf4e307d
rfablet <ronan.fablet@imt-atlantique.fr>
2023-05-23 10:57:48 +0200

    xxx

--- src/solver_l63.py
+++ src/solver_l63.py
@@ -687,7 +687,7 @@ class GradSolver_with_state_rnd(nn.Module):
             )
         
         print(' %e -- %e'%( np.sqrt( np.mean(var_cost_grad.detach().cpu().numpy()**2 ) ) ,
-                            1. / (normgrad) * np.sqrt( np.mean(var_cost_grad.detach().cpu().numpy()**2 ) )) )        
+                            1. / (normgrad_) * np.sqrt( np.mean(var_cost_grad.detach().cpu().numpy()**2 ) )) )        
                             
         x_k_plus_1 = x_k - state_update
         
