cc7d123dce2d3be35eccb40279c705ee922b2990
rfablet <ronan.fablet@imt-atlantique.fr>
2023-06-07 22:38:13 +0200

    xxx

--- src/test.py
+++ src/test.py
@@ -141,6 +141,23 @@ def base_testing(trainer, dm, lit_mod,ckpt,num_members=1):
     print('.. Variance among members runs             : %.3f'%var_rec)
     print('.. Maximum absolute difference between 2 runs : %.3f'%max_diff)
     
+    
+    # metrics for the mean among members
+    mean_x_rec = mean_x_rec.squeeze()
+    mse = np.mean( (mean_x_rec-X_test) **2 ) 
+    mse_i   = np.mean( (1.-mask_test.squeeze()) * (x_rec-X_test) **2 ) / np.mean( (1.-mask_test) )
+    mse_r   = np.mean( mask_test.squeeze() * (x_rec-X_test) **2 ) / np.mean( mask_test )
+    
+    nmse = mse / var_test
+    nmse_i = mse_i / var_test
+    nmse_r = mse_r / var_test
+    
+    print("..... Performance metrics, mean member(test data)")
+    print(".. MSE ALL.   : %.3f / %.3f"%(mse,nmse))
+    print(".. MSE ObsData: %.3f / %.3f"%(mse_r,nmse_r))
+    print(".. MSE Interp : %.3f / %.3f"%(mse_i,nmse_i))     
+    
+    
     # saving dataset
     result_path = ckpt.replace('.ckpt','_res.nc')
     x_test_obs = lit_mod.x_obs[:,:,cfg_params.dt_mse_test:x_train.shape[2]-cfg_params.dt_mse_test]
