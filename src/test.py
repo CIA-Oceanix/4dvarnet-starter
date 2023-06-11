4547a8fc2f77292af797c35b8c050b064c57d74b
rfablet <ronan.fablet@imt-atlantique.fr>
2023-06-11 09:04:21 +0200

    xxx

--- src/test.py
+++ src/test.py
@@ -49,8 +49,8 @@ def base_testing(trainer, dm, lit_mod,ckpt,num_members=1):
 #    print(lit_mod.model.model_H,flush=True)
     
     print('............... Model evaluation on validation dataset')
-    trainer.test(lit_mod, dataloaders=dm.val_dataloader())#, ckpt_path=ckpt)
-    #lit_mod = lit_mod.load_from_checkpoint(ckpt)
+    #trainer.test(lit_mod, dataloaders=dm.val_dataloader())#, ckpt_path=ckpt)
+    lit_mod = lit_mod.load_from_checkpoint(ckpt)
 
     print('...... cfg parameters from chekpoint',flush=True)
     print(lit_mod.hparams)
