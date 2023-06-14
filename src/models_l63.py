126e18dd0292a0a2cc466231b7c5be380e2fc3aa
rfablet <ronan.fablet@imt-atlantique.fr>
2023-06-14 22:17:54 +0200

    xxx

--- src/models_l63.py
+++ src/models_l63.py
@@ -548,6 +548,41 @@ class Phi_unet_1_layer(torch.nn.Module):
         x = x.view(-1,self.shapeData[0],self.shapeData[1],1)
         return x
 
+class Phi_unet_1_layer_bis(torch.nn.Module):
+    def __init__(self,shapeData,DimAE,dW=5):
+        super(Phi_unet_1_layer_bis, self).__init__()
+        self.pool1  = torch.nn.AvgPool2d((4,1))
+
+        self.conv_lr1  = torch.nn.Conv2d(shapeData[0],2*shapeData[0]*DimAE,(2*dW+1,1),padding=(dW,0),bias=False)
+        self.conv_lr2  = torch.nn.Conv2d(2*shapeData[0]*DimAE,shapeData[0]*DimAE,1,padding=0,bias=False)
+        
+        self.conv_lr3  = torch.nn.Conv2d(shapeData[0]*DimAE,2*shapeData[0]*DimAE,(2*dW+1,1),padding=(dW,0),bias=False)
+        self.conv_lr4  = torch.nn.Conv2d(2*shapeData[0]*DimAE,shapeData[0],1,padding=0,bias=False)
+
+        self.conv_hr1 = torch.nn.Conv2d(shapeData[0]*DimAE,2*shapeData[0]*DimAE,1,padding=0,bias=False)
+        self.conv_hr2 = torch.nn.Conv2d(2*shapeData[0]*DimAE,shapeData[0]*DimAE,1,padding=0,bias=False)
+        self.conv_hr3 = torch.nn.Conv2d(shapeData[0]*DimAE,2*shapeData[0]*DimAE,1,padding=0,bias=False)
+        self.conv_hr4  = torch.nn.Conv2d(2*shapeData[0]*DimAE,shapeData[0]*DimAE,1,padding=0,bias=False)
+
+        self.conv2Tr = torch.nn.ConvTranspose2d(shapeData[0]*DimAE,shapeData[0]*DimAE,(4,1),stride=(4,1),bias=False)          
+
+        self.shapeData = shapeData
+
+        self.model_name='unet-one-layer'
+    def forward(self, xinp):
+
+        # HR block
+        x = self.conv_hr2( F.relu(self.conv_hr1( self.pool1( xinp ) )) )
+        x = self.conv_hr4( F.relu( self.conv_hr3( x ) ) )        
+        x = self.conv2Tr( x )
+        
+        # LR block
+        x_lr = self.conv_lr2( F.relu( self.conv_lr1(xinp) ) )
+        x = x + self.conv_lr4( F.relu( self.conv_lr3(x_lr) ) )
+         
+        x = x.view(-1,self.shapeData[0],self.shapeData[1],1)
+        return x
+    
 class DoubleConv(torch.nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
