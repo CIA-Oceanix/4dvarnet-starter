daf7502ed5a7aad7d54a6aab1a0779d28436353d
rfablet <ronan.fablet@imt-atlantique.fr>
2023-06-14 10:00:33 +0200

    xxx

--- src/models_l63.py
+++ src/models_l63.py
@@ -15,7 +15,7 @@ import copy
 import torch
 import torch.optim as optim
 from torch.optim import lr_scheduler
-import torch.nn.functional as F
+import torch.nn.functional as nn
 from netCDF4 import Dataset
 
 from sklearn import decomposition
@@ -452,9 +452,9 @@ class Phi_ode(torch.nn.Module):
         
         return xnew
 
-class Phi_unet(torch.nn.Module):
+class Phi_unet_like(torch.nn.Module):
     def __init__(self,shapeData,DimAE,dW=5):
-        super(Phi_unet, self).__init__()
+        super(Phi_unet_like, self).__init__()
         self.pool1  = torch.nn.AvgPool2d((4,1))
         #self.conv1  = ConstrainedConv2d(shapeData[0],2*shapeData[0]*DimAE,(2*dW+1,1),padding=(dW,0),bias=False)
         self.conv1  = torch.nn.Conv2d(shapeData[0],2*shapeData[0]*DimAE,(2*dW+1,1),padding=(dW,0),bias=False)
@@ -487,7 +487,7 @@ class Phi_unet(torch.nn.Module):
         #x = self.pool1( xinp )
         x = self.pool1( xinp )
         x = self.conv1( x )
-        x = self.conv2( F.relu(x) )
+        x = self.conv2( nn.relu(x) )
         x = torch.cat((self.conv21(x), self.conv22(x) * self.conv23(x)),dim=1)
         x = self.conv3( x )
         x = self.conv2Tr( x )
@@ -495,7 +495,7 @@ class Phi_unet(torch.nn.Module):
         #x = self.conv6( F.relu(x) )
         
         xHR = self.convHR1( xinp )
-        xHR = self.convHR2( F.relu(xHR) )
+        xHR = self.convHR2( nn.relu(xHR) )
         xHR = torch.cat((self.convHR21(xHR), self.convHR22(xHR) * self.convHR23(xHR)),dim=1)
         xHR = self.convHR3( xHR )
         
@@ -504,6 +504,133 @@ class Phi_unet(torch.nn.Module):
         x = x.view(-1,self.shapeData[0],self.shapeData[1],1)
         return x
 
+
+class DoubleConv(torch.nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None,padding_mode='reflect',activation='relu',rateDropout=0.2):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+            
+        if activation == 'relu':
+            self.double_conv = nn.Sequential(
+                    nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False,padding_mode=padding_mode),
+                    nn.BatchNorm2d(mid_channels),
+                    nn.ReLU(inplace=True),
+                    nn.Dropout(rateDropout),
+                    nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False,padding_mode=padding_mode),
+                    nn.BatchNorm2d(out_channels),
+                    nn.ReLU(inplace=True)
+                )
+        elif activation == 'tanh' :
+            self.double_conv = nn.Sequential(
+                    nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False,padding_mode=padding_mode),
+                    nn.BatchNorm2d(mid_channels),
+                    nn.Tanh(inplace=True),
+                    nn.Dropout(rateDropout),
+                    nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False,padding_mode=padding_mode),
+                    nn.BatchNorm2d(out_channels),
+                    nn.Tanh(inplace=True) )
+        elif activation == 'logsigmoid' :
+            self.double_conv = nn.Sequential(
+                    nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False,padding_mode=padding_mode),
+                    nn.BatchNorm2d(mid_channels),
+                    nn.LogSigmoid(inplace=True),
+                    nn.Dropout(rateDropout),
+                    nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False,padding_mode=padding_mode),
+                    nn.BatchNorm2d(out_channels),
+                    nn.LogSigmoid(inplace=True) )
+#        elif activation == 'bilin' :
+#            self.double_conv = DoubleConvBILIN(in_channels, mid_channels,padding_mode=padding_mode)
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d((2,1)),
+            #nn.AvgPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=(2,1), mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=(2,1), stride=(2,1))
+            self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = 0#x2.size()[3] - x1.size()[3]
+
+        x1 = nn.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        # if you have padding issues, see
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+class UNet_3_layers(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=False):
+        super(UNet_3_layers, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 32)
+        self.down1 = Down(32, 64)
+        self.down2 = Down(64, 128)
+        #self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        
+        #self.down4 = Down(512, 1024 // factor)
+        #self.up1 = Up(1024, 512 // factor, bilinear)
+        #self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(128, 64 // factor, bilinear)
+        self.up4 = Up(64, 32, bilinear)
+        self.outc = OutConv(32, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        #x4 = self.down3(x3)
+        #x5 = self.down4(x4)
+        #x = self.up1(x5, x4)
+        #x5 = self.down4(x4)
+        #x = self.up1(x5, x4)
+        #print(x3.shape)
+        #print(x4.shape)
+        #x = self.up2(x4, x3)
+        x = self.up3(x3, x2)
+        x = self.up4(x, x1)
+        out = self.outc(x)
+        
+        return out
 
 class Model_HwithLocalisation(torch.nn.Module):
     def __init__(self,shape_data,kernel_t=5,sigma=1.):
@@ -689,7 +816,8 @@ class HParam:
 
 
 class Lit4dVarNet_L63(pl.LightningModule):
-    def __init__(self,ckpt_path=None,params=None,patch_weight=None,m_NormObs=None, m_NormPhi=None,mod_H=None,
+    def __init__(self,ckpt_path=None,params=None,patch_weight=None,
+                 Phi=None,m_NormObs=None, m_NormPhi=None,mod_H=None,
                  stats_training_data=None,*args, **kwargs):
         super().__init__()
         #self.hparams = HParam() if params is None else params
@@ -711,10 +839,9 @@ class Lit4dVarNet_L63(pl.LightningModule):
         self.hparams.automatic_optimization = True# False#
 
         # prior
-        if self.hparams.phi_param == 'ode':
-            Phi = Phi_ode()
-        elif self.hparams.phi_param == 'unet':    
-            Phi = Phi_unet(self.hparams.shapeData,self.hparams.DimAE)
+        if Phi == None :
+            Phi = Phi_unet_like(self.hparams.shapeData,self.hparams.DimAE)
+            
             
         if mod_H == None :
             mod_H = Model_H(self.hparams.shapeData)
