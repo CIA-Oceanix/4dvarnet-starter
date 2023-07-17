5e449e4037f00038b84752f5e520249de1f6940c
rfablet <ronan.fablet@imt-atlantique.fr>
2023-07-18 00:05:33 +0200

    xxx

--- src/models_l63.py
+++ src/models_l63.py
@@ -53,16 +53,14 @@ def get_constant_crop_l63(patch_dims, crop):
     patch_weight = patch_weight / np.sum(patch_weight)
     return patch_weight
 
-def get_forecasting_mask(patch_dims, dt_forecast, **kwargs):
-    patch_weight = np.zeros(patch_dims, dtype="float32")
-    
-    patch_weight = np.zeros((1,patch_dims[1],1), dtype="float32")
+def get_forecasting_mask(patch_dims, dt_forecast):
     w1 = np.arange(patch_dims[1]-dt_forecast).reshape((1,patch_dims[1]-dt_forecast,1))
     patch_weight = np.concatenate((w1,w1[-1]+np.ones((1,dt_forecast,1))),axis=1) 
     
     patch_weight = np.tile(patch_weight,(patch_dims[0],1,patch_dims[2]))
     patch_weight = patch_weight / np.sum(patch_weight)
 
+    print(patch_weight[0,:,0])
     return  patch_weight   
     
 def get_forecastingonly_mask(patch_dims, dt_forecast):
@@ -73,7 +71,6 @@ def get_forecastingonly_mask(patch_dims, dt_forecast):
     patch_weight = np.tile(patch_weight,(patch_dims[0],1,patch_dims[2]))
     patch_weight = patch_weight / np.sum(patch_weight)
 
-    print(patch_weight[0,:,0])
     return  patch_weight   
     
 if 1*0:
