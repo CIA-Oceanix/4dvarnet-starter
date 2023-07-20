a1e7afbc5b71bfcb06a76e18ef7bd394c67c295e
rfablet <ronan.fablet@imt-atlantique.fr>
2023-07-20 13:20:56 +0200

    xxx

--- src/models_l63.py
+++ src/models_l63.py
@@ -1156,12 +1156,13 @@ class Lit4dVarNet_L63(pl.LightningModule):
         self.model.lr_grad = self.hparams.lr_grad
 
         self._set_norm_stats()
+        
     def extract_data_patch(self,batch):
         return batch
     
     def training_step(self, train_batch, batch_idx, optimizer_idx=0):
         
-        train_batch = self.extract_data_patch( train_batch )
+        #train_batch = self.extract_data_patch( train_batch )
         
         opt = self.optimizers()
         inputs_init,inputs_obs,masks,targets_GT = train_batch
