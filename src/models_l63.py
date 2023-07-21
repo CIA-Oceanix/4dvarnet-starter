eb3ec49962df977756b5d55bbb08d2d93b04bd9a
rfablet <ronan.fablet@imt-atlantique.fr>
2023-07-21 16:28:24 +0200

    xxx

--- src/models_l63.py
+++ src/models_l63.py
@@ -1491,8 +1491,8 @@ class Lit4dVarNet_L63_OdeSolver(Lit4dVarNet_L63):
         
         if self.hparams.integration_step > 1 :
             print(rec[0,0,:].detach().cpu().numpy().transpose())
-            rec = torch.nn.functional.interpolate(rec, scale_factor=(self.hparams.integration_step,1), mode='bicubic')#,align_corners=True)#, align_corners=None, recompute_scale_factor=None, antialias=False)                
-            print(rec[0,0,:].detach().cpu().numpy().transpose())
+            rec_ = torch.nn.functional.interpolate(rec, scale_factor=(self.hparams.integration_step,1), mode='bicubic')#,align_corners=True)#, align_corners=None, recompute_scale_factor=None, antialias=False)                
+            print(rec_[0,0,:].detach().cpu().numpy().transpose())
             rec = torch.nn.functional.interpolate(rec, scale_factor=(self.hparams.integration_step,1), mode='bicubic',align_corners=True)#, align_corners=None, recompute_scale_factor=None, antialias=False)                
             print(rec[0,0,:].detach().cpu().numpy().transpose())
             print()
