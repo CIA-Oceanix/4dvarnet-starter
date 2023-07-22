1516639da00dc47a7af1d833346363e7f523086f
rfablet <ronan.fablet@imt-atlantique.fr>
2023-07-22 23:48:37 +0200

    xxx

--- src/test.py
+++ src/test.py
@@ -420,7 +420,7 @@ def base_testing_ode_solver(trainer, dm, lit_mod,ckpt=None,num_members=1):
     idx_val = X_train.shape[0]-500
     
     X_val = X_train[idx_val::,:,:8]
-    mask_val = mask_train[idx_val::,:,:,:].squeeze()
+    mask_val = mask_train[idx_val::,:,:,:8].squeeze()
     x_rec = lit_mod.x_rec#[:,:,cfg_params.dt_mse_test:x_train.shape[2]-cfg_params.dt_mse_test]
     x_ode = lit_mod.x_ode
     
@@ -452,7 +452,7 @@ def base_testing_ode_solver(trainer, dm, lit_mod,ckpt=None,num_members=1):
     x_ode = lit_mod.x_ode
 
     X_test = X_test[:,:,:8]
-    
+    mask_test = mask_test[:,:,:8]
 
     var_test  = np.mean( (X_test - np.mean(X_test,axis=0))**2 )
     mse = np.mean( (x_rec-X_test) **2 ) 
