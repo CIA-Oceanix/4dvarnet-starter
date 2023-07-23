6e2706b5cb2537423723f6dc59737f8dd0aeafd3
rfablet <ronan.fablet@imt-atlantique.fr>
2023-07-23 22:46:45 +0200

    xxx

--- src/test.py
+++ src/test.py
@@ -470,7 +470,7 @@ def base_testing_ode_solver(trainer, dm, lit_mod,ckpt=None,num_members=1):
     rmse = np.sqrt( np.mean( (X_test[:,:,X_test.shape[2]-lit_mod.hparams.dt_forecast:]-x_rec[:,:,X_test.shape[2]-lit_mod.hparams.dt_forecast:])**2 ) )
     print(".. rmse all: %.3f "%rmse)
     for tt in range(X_test.shape[2]):
-        dt = tt - (X_test.shape[2]-(lit_mod.hparams.dt_forecast+1)*(lit_mod.hparams.integration_step)
+        dt = tt - (X_test.shape[2]-(lit_mod.hparams.dt_forecast+1)*(lit_mod.hparams.integration_step))
         rmse = np.sqrt( np.mean( (X_test[:,:,tt]-x_rec[:,:,tt] )**2 ) )
         rmse_ode = np.sqrt( np.mean( (X_test[:,:,tt]-x_ode[:,:,tt] )**2 ) )
         print(".. dt = %d -- rmse = %.3f -- %.3f"%(dt,rmse_ode,rmse))
