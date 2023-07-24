b1d7a61ba5c16e88869d739f6b0ee45e890438bf
rfablet <ronan.fablet@imt-atlantique.fr>
2023-07-24 15:55:03 +0200

    xxx

--- src/test.py
+++ src/test.py
@@ -416,7 +416,7 @@ def base_testing_ode_solver(trainer, dm, lit_mod,ckpt=None,num_members=1):
     
     # time windows
     dT_hr = lit_mod.hparams.shapeData[1]*lit_mod.hparams.integration_step
-    t_last_obs_hr = lit_mod.hparams.shapeData[1]-(lit_mod.hparams.dt_forecast+1)*lit_mod.hparams.integration_step
+    t_last_obs_hr = lit_mod.hparams.shapeData[1]*lit_mod.hparams.integration_step-(lit_mod.hparams.dt_forecast+1)*lit_mod.hparams.integration_step
     t_forecast_hr = (lit_mod.hparams.dt_forecast+1)*lit_mod.hparams.integration_step-1
     
     
