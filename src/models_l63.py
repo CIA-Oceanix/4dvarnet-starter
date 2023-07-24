fd4cff84c62f9b3c6058712da4e5c9da109fbb09
rfablet <ronan.fablet@imt-atlantique.fr>
2023-07-24 16:52:22 +0200

    xxx

--- src/models_l63.py
+++ src/models_l63.py
@@ -1782,8 +1782,7 @@ class Lit4dVarNet_L63_OdeSolver(Lit4dVarNet_L63):
             delta_t0 = (self.hparams.dt_forecast+1)*self.hparams.integration_step-1
             out_all_seq_hr = None
             
-            for t0 in range(0,self.hparams.dT_test*self.hparams.integration_step,delta_t0):                                    
-                print(t0)
+            for t0 in range(0,self.hparams.dT_test*self.hparams.integration_step-delta_t0,delta_t0):                                    
                 if t0 > 0 :
                     inputs_init,inputs_obs,masks,targets_GT = test_batch
 
