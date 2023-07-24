ead70ef88a1b842a17d81e752c169c1787f4c0ed
rfablet <ronan.fablet@imt-atlantique.fr>
2023-07-24 13:18:37 +0200

    xxx

--- src/models_l63.py
+++ src/models_l63.py
@@ -1859,8 +1859,8 @@ class Lit4dVarNet_L63_OdeSolver(Lit4dVarNet_L63):
                 y0 = inputs_init_[:,:,inputs_init_.size(2)-self.hparams.dt_forecast-1].view(-1,inputs_init_.size(1),1)
                 y0 = targets_GT[:,:,0].view(-1,inputs_init_.size(1),1)
                 
-                print( inputs_init_[0,:,0].detach().cpu().numpy().transpose() )
-                print( targets_GT[0,:,0].detach().cpu().numpy().transpose() )
+                print( inputs_init_[0,:,0].detach().cpu().numpy().transpose() * self.stdTr + self.meanTr )
+                print( targets_GT[0,:,0].detach().cpu().numpy().transpose() * self.stdTr  + self.meanTr )
                 
                 x_pred = self.ode_solver.solve_from_initial_condition(y0,self.hparams.dt_forecast*self.hparams.integration_step+1)                    
 
