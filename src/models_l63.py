02590dea349cc0d7ddea1ed6b7abe631311bde05
rfablet <ronan.fablet@imt-atlantique.fr>
2023-07-26 18:06:30 +0200

    xxx

--- src/models_l63.py
+++ src/models_l63.py
@@ -1271,7 +1271,7 @@ class Lit4dVarNet_L63(pl.LightningModule):
         #print(hparams,flush=True)
         
         
-        #self.save_hyperparameters(params)
+        self.save_hyperparameters(params)
 
         self.params = params
        
