forecast global, masking fix, moving patches, ...

reproduce/reproduce_utils.py
@@ -23,7 +23,7 @@ def reconstruct_from_config(config, your_rec_dir, best_ckpt_path):
         inference_mode= False,
         accelerator='gpu',
         devices= 1,
-        logger = pytorch_lightning.loggers.CSVLogger(save_dir= your_rec_dir, name= "reproduce")
+        logger = pytorch_lightning.loggers.CSVLogger(save_dir= '/DATASET/GLORYS12/outputs/'+your_rec_dir, name= "reproduce")
         )
 
     lit_mod = call_cfg_key(config, 'model')
@@ -189,7 +189,6 @@ def interp_on_alongtrack(gridded_dataset,
     lat_alongtrack = ds_alongtrack["lat"].values
     time_alongtrack = ds_alongtrack["time"].values
 
-    #print('time along track len before masking: {}'.format(time_alongtrack.shape))
 
     # get and apply mask from map_interp & alongtrack on each dataset
     msk1 = np.ma.masked_invalid(ssh_alongtrack).mask
@@ -608,6 +607,10 @@ def eval_ose(path_alongtrack,
              var_name="out",
              time_min='2017-01-01',
              time_max='2017-12-31',
+             lon_min = -65.,
+             lon_max = -55.,
+             lat_min = 33.,
+             lat_max = 43.,
              centered=False,
              plot_scores=False):
     """
@@ -631,10 +634,10 @@ def eval_ose(path_alongtrack,
                              centered = False)
     """
     # Study area
-    lon_min = -65.
-    lon_max = -55.
-    lat_min = 33.
-    lat_max = 43.
+    #lon_min = -65.
+    #lon_max = -55.
+    #lat_min = 33.
+    #lat_max = 43.
     is_circle = False
 
     # Outputs
@@ -659,7 +662,6 @@ def eval_ose(path_alongtrack,
     ds_alongtrack = read_l3_dataset(path_alongtrack, lon_min, lon_max, lat_min,
                                     lat_max, time_min, time_max, centered)
     
-    #print('ds alongtrack time dim len: {}'.format(ds_alongtrack.time.values.shape))
 
     # Read reconstructed datasets and interpolate onto alongtrack positions
     time_alongtrack, lat_alongtrack, lon_alongtrack, ssh_alongtrack, ssh_map_interp = interp_on_alongtrack(
@@ -686,15 +688,20 @@ def eval_ose(path_alongtrack,
                                 ssh_map_interp, lenght_scale, delta_x, delta_t, "spectrum.nc")
         learderboard_psds_score = -999
         learderboard_psds_score = plot_psd_score("spectrum.nc", plot=plot_scores)
+        print(learderboard_psds_score)
     except OverflowError:
         learderboard_psds_score = np.nan
+        print('overflow: {}'.format(learderboard_psds_score))
 
     os.remove("spectrum.nc")
 
-    return leaderboard_nrmse, int(learderboard_psds_score)
+    # WARNING: LEADERBOARD PSD SHOULS BE CAST TO INT
 
+    return leaderboard_nrmse, learderboard_psds_score
+
+from matplotlib import ticker
 # plot results
-def plot_results(RMSE_array, save_name=None):
+def plot_results(RMSE_array, save_name=None, plot_baked=True, vmin=None, vmax=None, name=None):
     x_7days = range(7)
     x_8days = range(-1, 7)
     x_glo_persistence = range(5)
@@ -734,46 +741,53 @@ def plot_results(RMSE_array, save_name=None):
     ax = plt.subplot(111)
 
     ax.plot(x_7days,
-            y_4dvarnet[13:],
-            label="4DVarNet",
-            color='blue')
-    ax.plot(x_7days,
             RMSE_array,
-            label="4DVarNet trained on GLORYS",
+            label="4DVarNet trained on GLORYS" if name is None else name,
             color='red',)
-    """ax.plot(x_7days,
-            y_4dvarnet_mapping * np.ones_like(x_7days),
-            label="4DVarNet mapping",
-            color='blue',
-            linestyle='dashed')"""
-    ax.plot(x_7days,
-            y_glo[:7],
-            label="Glo",
-            color='green')
-    """ax.plot(x_glo_persistence,
-            y_glo_persistence,
-            label="Glo persistence",
-            color='green',
-            linestyle='dashdot')"""
-    ax.plot(x_7days,
-            y_glorys * np.ones_like(x_7days),
-            label="Glorys",
-            color='green',
-            linestyle='dashed')
-    ax.plot(x_7days,
-            y_oi * np.ones_like(x_7days),
-            label="Duacs mapping",
-            color='orange',
-            linestyle='dashed')
-    """ax.plot(x_7days,
-            y_duacs_persistence[1:],
-            label="Duacs persistence",
-            color='orange',
-            linestyle='dashdot')"""
+    
+    if plot_baked:
+        ax.plot(x_7days,
+                y_4dvarnet[13:],
+                label="4DVarNet",
+                color='blue')
+        """ax.plot(x_7days,
+                y_4dvarnet_mapping * np.ones_like(x_7days),
+                label="4DVarNet mapping",
+                color='blue',
+                linestyle='dashed')"""
+        ax.plot(x_7days,
+                y_glo[:7],
+                label="Glo",
+                color='green')
+        """ax.plot(x_glo_persistence,
+                y_glo_persistence,
+                label="Glo persistence",
+                color='green',
+                linestyle='dashdot')"""
+        ax.plot(x_7days,
+                y_glorys * np.ones_like(x_7days),
+                label="Glorys",
+                color='green',
+                linestyle='dashed')
+        ax.plot(x_7days,
+                y_oi * np.ones_like(x_7days),
+                label="Duacs mapping",
+                color='orange',
+                linestyle='dashed')
+        """ax.plot(x_7days,
+                y_duacs_persistence[1:],
+                label="Duacs persistence",
+                color='orange',
+                linestyle='dashdot')"""
 
+
+    ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
     plt.xlabel("Day of prediction")
     plt.ylabel("Average nRMSE score")
     plt.grid(alpha=.3)
+
+    if vmin is not None and vmax is not None:
+        ax.set_ylim([vmin, vmax])
 
     ax.legend(bbox_to_anchor=(1.04, 0.5), loc='center left', borderaxespad=0)
     if save_name is not None:

src/data.py
@@ -7,6 +7,7 @@ import functools as ft
 from collections import namedtuple
 import time
 import cv2
+from tqdm import tqdm
 
 TrainingItem = namedtuple('TrainingItem', ['input', 'tgt'])
 TrainingItemMask = namedtuple('TrainingItemMask', ['input', 'tgt', 'mask'])
@@ -53,17 +54,31 @@ class XrDataset(torch.utils.data.Dataset):
         self.return_coords = False
         self.postpro_fn = postpro_fn
         self.da = da.sel(**(domain_limits or {}))
+        self.domain_limits = domain_limits
         self.patch_dims = patch_dims
         self.strides = strides or {}
         da_dims = dict(zip(self.da.dims, self.da.shape))
-        self.ds_size = {
-            dim: max((da_dims[dim] - patch_dims[dim]) // self.strides.get(dim, 1) + 1, 0)
-            for dim in patch_dims
-        }
+        self.da_dims = da_dims
+        self.define_ds_size(da_dims, patch_dims, self.strides)
         self.test_regrid = test_regrid
 
-        if check_full_scan:
-            for dim in patch_dims:
+        self.check_full_scan(check_full_scan, da_dims)
+        self.check_dim_order(check_dim_order)
+        
+        print('dataset patch sizes: {}'.format(self.ds_size))
+        print('dataset sizes: {}'.format(self.da.sizes))
+        print('dataset length: {}'.format(len(self)))
+        print('-'*40)
+
+    def define_ds_size(self, da_dims, patch_dims, strides):
+        self.ds_size = {
+            dim: max((da_dims[dim] - patch_dims[dim]) // strides.get(dim, 1) + 1, 0)
+            for dim in patch_dims
+        }
+
+    def check_full_scan(self, check, da_dims):
+        if check:
+            for dim in self.patch_dims:
                 if (da_dims[dim] - self.patch_dims[dim]) % self.strides.get(dim, 1) != 0:
                     raise IncompleteScanConfiguration(
                         f"""
@@ -74,21 +89,18 @@ class XrDataset(torch.utils.data.Dataset):
                         [shape - patch_size] should be divisible by stride
                         """
                     )
-
-        if check_dim_order:
-            for dim in patch_dims:
-                if not '#'.join(da.dims).endswith('#'.join(list(patch_dims))):
-                    raise DangerousDimOrdering(
-                        f"""
-                        input dataarray's dims should end with patch_dims
-                        dataarray's dim {da.dims}:
-                        patch_dims {list(patch_dims)}
-                        """
-                    )
-        
-        print('dataset patch sizes: {}'.format(self.ds_size))
-        print('dataset sizes: {}'.format(self.da.sizes))
-        print('dataset length: {}'.format(len(self)))
+            
+    def check_dim_order(self, check):
+        if check:
+            for dim in self.patch_dims:
+                    if not '#'.join(self.da.dims).endswith('#'.join(list(self.patch_dims))):
+                        raise DangerousDimOrdering(
+                            f"""
+                            input dataarray's dims should end with patch_dims
+                            dataarray's dim {self.da.dims}:
+                            patch_dims {list(self.patch_dims)}
+                            """
+                        )
 
     def __len__(self):
         size = 1
@@ -120,7 +132,7 @@ class XrDataset(torch.utils.data.Dataset):
         item = self.da.isel(**sl)
 
         if self.return_coords:
-            return item.coords.to_dataset().drop_vars('depth')[list(self.patch_dims)]
+            return item.coords.to_dataset()[list(self.patch_dims)]
 
         item = item.data.astype(np.float32)
         if self.postpro_fn is not None:
@@ -140,16 +152,33 @@ class XrDataset(torch.utils.data.Dataset):
 
         items = list(itertools.chain(*batches))
         return self.reconstruct_from_items(items, weight)
+    
+    def rec_crop_valid(self, da, coords):
+        da_slice = {}
+        for dim in da.dims:
+            if dim in coords.dims:
+                da_slice[dim] = slice(0,coords.sizes[dim])
+        return da.isel(da_slice)
 
     def reconstruct_from_items(self, items, weight=None):
         if weight is None:
+            print('WEIGHT IS NONE ---------------------')
             weight = np.ones(list(self.patch_dims.values()))
         w = xr.DataArray(weight, dims=list(self.patch_dims.keys()))
 
         coords = self.get_coords()
-
+        
         new_dims = [f'v{i}' for i in range(len(items[0].shape) - len(coords[0].dims))]
         dims = new_dims + list(coords[0].dims)
+
+        for idx in range(len(items)):
+            it_slice = []
+            for i_dim, dim in enumerate(dims):
+                if dim in coords[0].dims:
+                    it_slice.append(slice(0,coords[idx].sizes[dim]))
+                else:
+                    it_slice.append(slice(None))
+            items[idx] = items[idx][it_slice]
 
         das = [xr.DataArray(it.numpy(), dims=dims, coords=co.coords)
                for it, co in zip(items, coords)]
@@ -165,10 +194,60 @@ class XrDataset(torch.utils.data.Dataset):
         count_da = xr.zeros_like(rec_da)
 
         for da in das:
-            rec_da.loc[da.coords] = rec_da.sel(da.coords) + da * w
-            count_da.loc[da.coords] = count_da.sel(da.coords) + w
+            rec_da.loc[da.coords] = rec_da.sel(da.coords) + da.sel(da.coords) * self.rec_crop_valid(w, da.coords)
+            count_da.loc[da.coords] = count_da.sel(da.coords) + self.rec_crop_valid(w, da.coords)
 
         return rec_da / count_da
+    
+class XrDatasetMovingPatch(XrDataset):
+    
+    def __init__(self, *args, rand=True, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rand = rand
+
+    def define_ds_size(self, da_dims, patch_dims, strides):
+        ds_size = {}
+        patch_offset = {}
+        for dim in patch_dims:
+            dim_size = (da_dims[dim] - patch_dims[dim]) // strides.get(dim, 1) + 1
+            patch_offset[dim] = (da_dims[dim] - patch_dims[dim]) % strides.get(dim, 1)
+            if patch_offset[dim] != 0:
+                dim_size += 1
+            ds_size[dim] = max(dim_size, 0)
+        self.ds_size = ds_size
+        self.patch_offset = patch_offset
+
+
+    def __getitem__(self, item):
+        sl = {
+            dim: slice(self.strides.get(dim, 1) * idx,
+                       self.strides.get(dim, 1) * idx + self.patch_dims[dim])
+            for dim, idx in zip(self.ds_size.keys(),
+                                np.unravel_index(item, tuple(self.ds_size.values())))
+        }
+
+        # moving patch
+        ds_overflow = {}
+        for dim in ['lat', 'lon']:
+            patch_offset = self.get_patch_offset(dim)
+            sl[dim] = slice(sl[dim].start + patch_offset, sl[dim].stop + patch_offset)
+            ds_overflow[dim] = sl[dim].stop - self.da_dims[dim]
+            sl[dim] = slice(min(sl[dim].start, self.da_dims[dim]), min(sl[dim].stop, self.da_dims[dim]))
+
+        item = self.da.isel(**sl)
+        if self.return_coords:
+            return item.coords.to_dataset()[list(self.patch_dims)]
+        item = item.pad({dim: (0, dim_overflow if dim_overflow>0 else 0) for dim, dim_overflow in ds_overflow.items()}, mode='constant', constant_values=np.nan)
+
+
+        item = item.data.astype(np.float32)
+        if self.postpro_fn is not None:
+            item = self.postpro_fn(item)
+
+        return item
+    
+    def get_patch_offset(self, dim):
+        return np.random.randint(0, self.patch_offset[dim]) if self.rand else 0
 
 class XrDatasetRegrid(torch.utils.data.Dataset):
     """
@@ -401,7 +480,6 @@ class BaseDataModule(pl.LightningDataModule):
         self.test_ds = None
         self._post_fn = None
         self.test_regrid = test_regrid
-        print('test_regrid: {}'.format(test_regrid))
 
     def norm_stats(self):
         if self._norm_stats is None:
@@ -577,3 +655,24 @@ class RandValDataModule(BaseDataModule):
             self.train_ds = AugmentedDataset(self.train_ds, **self.aug_kw)
 
         self.test_ds = XrDataset(self.input_da.sel(self.domains['test']), **self.xrds_kw, postpro_fn=post_fn,)
+
+class MovingPatchDataModule(BaseDataModule):
+    def __init__(self, *args, rec_crop=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rec_crop = rec_crop
+
+    def setup(self, stage='test'):
+        train_data = self.input_da.sel(self.domains['train'])
+        post_fn = self.post_fn()
+        self.train_ds = XrDatasetMovingPatch(
+            train_data, **self.xrds_kw, postpro_fn=post_fn, test_regrid=self.test_regrid, rand=True
+        )
+        if self.aug_kw:
+            self.train_ds = AugmentedDataset(self.train_ds, **self.aug_kw)
+
+        self.val_ds = XrDatasetMovingPatch(
+            self.input_da.sel(self.domains['val']), **self.xrds_kw, postpro_fn=post_fn, test_regrid=self.test_regrid, rand=False
+        )
+        self.test_ds = XrDatasetMovingPatch(
+            self.input_da.sel(self.domains['test']), **self.xrds_kw, postpro_fn=post_fn, test_regrid=self.test_regrid, rand=False
+        )

src/models.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from src.data import TrainingItemMask
+from tqdm import tqdm
 
 from src.utils import regrid_interp
 
@@ -175,21 +176,23 @@ class Lit4dVarNetForecast(Lit4dVarNet):
     loss_masking: will mask nan values present in the target for loss and gradient computations
     """
 
-    def __init__(self, solver, rec_weight, opt_fn, test_metrics=None, pre_metric_fn=None, norm_stats=None, persist_rw=True, test_regrid=None, loss_masking=True):
+    def __init__(self, solver, rec_weight, opt_fn, test_metrics=None, pre_metric_fn=None, norm_stats=None, persist_rw=True, test_regrid=None, loss_masking=True, output_only_forecast=True):
         super().__init__(solver, rec_weight, opt_fn, test_metrics, pre_metric_fn, norm_stats, persist_rw, test_regrid, loss_masking)
+        self.output_only_forecast=output_only_forecast
 
     @staticmethod
     def mask_batch(batch, loss_masking):
+
+        # temporal masking
         new_input = batch.input
         dims = new_input.size()
-        new_input[:, dims[1]//2:, :, :] = 0.
-        mask_batch = batch._replace(input=new_input)
-        mask_batch = mask_batch._replace(input=(batch.input).nan_to_num())
-        if loss_masking:
-            mask_batch = mask_batch._replace(mask=(batch.tgt).isfinite().clone().type(torch.bool))
-        else:
-            mask_batch = mask_batch._replace(mask=torch.full_like(batch.tgt, True).type(torch.bool))
-        mask_batch = mask_batch._replace(tgt=(batch.tgt).nan_to_num())
+        new_input[:, dims[1]//2:, :, :] = float('nan')
+
+        # batch.mask
+        new_mask = (batch.tgt).isfinite() if loss_masking else torch.full_like(batch.tgt, True).type(torch.bool)
+
+        mask_batch = batch._replace(input=new_input, mask=new_mask)
+
         return mask_batch
 
     def training_step(self, batch, batch_idx):
@@ -208,10 +211,12 @@ class Lit4dVarNetForecast(Lit4dVarNet):
         dims = self.rec_weight.size()
         dT = dims[0]
         metrics = []
-        for i in range(-((dT - 1) // 2 - 1), 7):
+        output_start = 0 if self.output_only_forecast else -((dT - 1) // 2 - 1)
+        for i in tqdm(range(output_start, 8)):
             forecast_weight = np.concatenate(
                 (np.zeros((dT // 2 + i, dims[1], dims[2])),
-                 np.ones((1, dims[1], dims[2])),
+                 # using rec_weight to crop results accordingly
+                 np.expand_dims(self.rec_weight.cpu().numpy()[dT//2], axis=0),
                  np.zeros((dT // 2 - i, dims[1], dims[2]))),
                 axis=0)
             rec_da = self.trainer.test_dataloaders.dataset.reconstruct(
@@ -237,7 +242,7 @@ class Lit4dVarNetForecast(Lit4dVarNet):
             })
             metrics.append(metrics_leadtime)
 
-        print(pd.DataFrame(metrics, range(-((dT - 1) // 2 - 1), 7)).T.to_markdown())
+        print(pd.DataFrame(metrics, range(output_start, 8)).T.to_markdown())
 
 
 

src/utils.py
@@ -133,7 +133,7 @@ def get_triang_time_wei(patch_dims, offset=0, **crop_kw):
     )
 
 
-def get_forecast_wei(patch_dims, **crop_kw):
+def get_forecast_wei(patch_dims, flat_end=False, exponential_ascension=False, **crop_kw):
     """
     return weight for forecast reconstruction:
     patch_dims: dimension of the patches used
@@ -144,8 +144,8 @@ def get_forecast_wei(patch_dims, **crop_kw):
     """
     pw = get_constant_crop(patch_dims, **crop_kw)
     time_patch_weight = np.concatenate(
-        (np.linspace(0, 1, (patch_dims['time'] - 1) // 2),
-         np.linspace(1, 0.5, 7),
+        (np.power(np.linspace(0, 1, (patch_dims['time'] - 1) // 2), 2 if exponential_ascension else 1),
+         np.linspace(1, 1, 7) if flat_end else np.linspace(1, 0.5, 7),
          np.zeros((patch_dims['time'] + 1) // 2 - 7)),
         axis=0)
     final_patch_weight = time_patch_weight[:, None, None] * pw
@@ -197,11 +197,44 @@ def load_ose_data(path):
         .assign(
             input=lambda ds: ds.ssh,
             tgt=lambda ds: ds.ssh,
+            mask=lambda ds: ds.ssh,
         )
     )
 
     return (
-        ds[[*src.data.TrainingItem._fields]]
+        ds[[*src.data.TrainingItemMask._fields]]
+        .transpose("time", "lat", "lon")
+        .to_array()
+    )
+
+def load_ose_data_with_tgt_mask(path, tgt_path, variable='zos', test_cut=None):
+
+    ds_mask =  (
+        xr.open_dataset(tgt_path).drop_vars('depth')
+    )
+
+    ds = xr.open_dataset(path)
+
+    if 'latitude' in list(ds_mask.dims):
+        ds_mask = ds_mask.rename({'latitude':'lat', 'longitude':'lon'})
+
+    ds_mask = ds_mask.sel(time='2020-01-20')[variable].expand_dims(time=ds.time).assign_coords(ds.coords)
+    #if test_cut is not None:
+        #ds = ds.sel(time=test_cut).load()
+
+    print('ds_s loaded')
+
+    ds = (
+        ds
+        .assign(
+            input=ds.ssh,
+            tgt=ds_mask,
+            mask=ds_mask,
+        )
+    )
+
+    return (
+        ds[[*src.data.TrainingItemMask._fields]]
         .transpose("time", "lat", "lon")
         .to_array()
     )
@@ -236,8 +269,9 @@ def mask_input(da, mask_list):
 def open_glorys12_data(path, masks_path, domain, variables="zos", masking=True, test_cut=None):
 
     print("LOADING input data")
+    # DROPPING DEPTH !!
     ds =  (
-        xr.open_dataset(path)
+        xr.open_dataset(path).drop_vars('depth')
     )
     
     if 'latitude' in list(ds.dims):
@@ -277,8 +311,6 @@ def open_glorys12_data(path, masks_path, domain, variables="zos", masking=True, 
         .transpose("time", "lat", "lon")
         .to_array()
     )
-
-    print('dataset sizes: {}'.format(ds.sizes))
 
     return ds
 