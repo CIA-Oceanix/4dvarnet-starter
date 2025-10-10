30e9681087eb549927f6f63969f445ec3587d1b5
Daria BOTVYNKO <d21botvy@sl-mee-br-116.imta.fr>
2025-10-10 13:36:16 +0200

    corrected rec contrib/data results with unet 0.9308481076984234 ldt0

--- contrib/data_loading/data.py
+++ contrib/data_loading/data.py
@@ -79,8 +79,9 @@ def load_ose_data_with_tgt_mask_SLA(path, tgt_path, tgt_path_not_glorys, tgt_pat
     print(f'tgt_path is {tgt_path}')
     print(f'path is {path}')
     #s_mask = xr.open_dataset('/Odyssey/public/glorys/reanalysis/glorys12_2020_daily_sla_4th.nc')
-    ds_mask = xr.open_dataset(tgt_path)#drop_vars('depth')    # TGT_PATH is GLORYS12_DATA in contrib/ose_pipeline/ose_rec_pipeline.py
-    #s_mask = xr.open_dataset('/Odyssey/public/duacs/2023/duacs_2020_2023_0.125deg.nc')
+    #s_mask = xr.open_dataset(tgt_path)#drop_vars('depth')    # TGT_PATH is GLORYS12_DATA in contrib/ose_pipeline/ose_rec_pipeline.py
+    #ds_mask = xr.open_dataset('/Odyssey/public/duacs/2023/duacs_2020_2023_0.125deg.nc') -> for all commits "REPRODUCE" with title "tgt DUACS" in rec folder
+    ds_mask = xr.open_dataset('/Odyssey/public/duacs/2023/duacs_2017_2022_0.25deg.nc')
     ds = xr.open_dataset(path)
 
     if 'latitude' in list(ds_mask.dims):
@@ -108,7 +109,7 @@ def load_ose_data_with_tgt_mask_SLA(path, tgt_path, tgt_path_not_glorys, tgt_pat
     #ds_mask = ds_mask.sel(time= str(year) + '-01-20')[variable].expand_dims(time=ds.time)[:,:,:] # 2024 for swot ?
     #ds_mask = ds_mask.sel(time= '2023' + '-01-20')[variable].expand_dims(time=ds.time)[:,:,:]#.assign_coords(ds.coords) # 2024 for swot ? # Just for the reproducibility test
     #ds_mask = ds_mask.sel(time= '2020' + '-01-20')[variable].expand_dims(time=ds.time)[:,:,:]
-    ds_mask = ds_mask.sel(time= '2020' + '-01-20')[variable].expand_dims(time=ds.time)[:,:,:1440].assign_coords(ds.coords)
+    ds_mask = ds_mask.sel(time= '2020' + '-01-20')[variable].expand_dims(time=ds.time)[:,40:,:1440].assign_coords(ds.coords)
     # IMPORTANT : #.assign_coords(ds.coords)
     print(ds_mask[0].shape)
     # BEOFRE SWOT : ds_mask.sel(time='2019-01-20')[variable].expand_dims(time=ds.time)[:,:,:]   # CHANGED FROM 2023 TO 2019 !!! , but should be 2020 ! # CHNAGED AGAIN FROM 2019 TO 2021  
